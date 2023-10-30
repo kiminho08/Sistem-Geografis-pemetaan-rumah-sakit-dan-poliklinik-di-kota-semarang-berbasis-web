868725d9b7a56995be97cd32e9da563442f0c203
Web Developer <108423746+kiminho08@users.noreply.github.com>
2023-10-30 15:44:17 +0700

    Add files via upload

---
+++ styles/RumahSakitTahun2018_2_style.js
@@ -0,0 +1,42 @@
+var size = 0;
+var placement = 'point';
+
+var style_RumahSakitTahun2018_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("R.SAKIT");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10.725px \'MS Shell Dlg 2\', sans-serif";
+    var labelFill = "rgba(0, 0, 0, 1)";
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("KECAMATAN") !== null) {
+        labelText = String(feature.get("KECAMATAN"));
+    }
+    if (value > 0.000000 && value <= 1.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(215,25,28,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })]
+                    } else if (value > 1.000000 && value <= 2.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(255,255,191,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })]
+                    } else if (value > 2.000000 && value <= 3.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(44,123,182,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })]
+                    };
+
+    return style;
+};
